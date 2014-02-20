b889d703307a93cbfb06d251e906cd0378401f17
unknown <mallen@NCCSC-BKL.cnrdom.ColoState.EDU>
2014-02-20 12:08:03 -0700

    deleting old code

--- RcodeOld/ClimateMaps.r
+++
@@ -1,6 +0,0 @@
-library(OpenStreetMap)
-library(shapefiles)
-map <- openmap(c(42,-112), c(45.5,-108),type="bing")
- plot(map)
-a<-readShapePoly("H:\\Desktop\\Climate\\ParkBoundaries\\NPSBoundaries.shp")
-
